36927673221bf113bf75313eb83adf704f6d692f
Andres_Delgado <inred.ad21@gmail.com>
2026-04-17 17:20:43 -0500

    first commit

---
+++ src/main/java/com/proxy/tallerProxy/TallerProxyApplication.java
@@ -0,0 +1,13 @@
+package com.proxy.tallerProxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TallerProxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TallerProxyApplication.class, args);
+	}
+
+}
